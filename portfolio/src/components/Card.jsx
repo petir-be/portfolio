9fe6f522e763231843d0c16b30b4df13e9b4b720
pits <pits@KURT>
2025-09-30 14:54:56 +0800

    hu

--- portfolio/src/components/Card.jsx
+++ portfolio/src/components/Card.jsx
@@ -3,7 +3,7 @@ import "../App.css"
 function Card({ image, title }) {
     return (
         <div class="relative w-full mb-6 group overflow-hidden rounded-md shadow-lg break-inside-avoid hover:-translate-y-1 transition duration-150">
-            <img src={image} alt={title} class="w-full h-auto object-cover min-w-52" />
+            <img src={image} alt={title} class="w-full h-auto object-cover" />
         </div>
     );
 }
